088416abffa74a15bad5f3c3f2f1b2e157a6fa89
Jia <jhan@physics.ucla.edu>
2024-02-14 19:38:06 -0800

    Update routine.ino

--- arduino_src/routine.ino
+++ arduino_src/routine.ino
@@ -3,40 +3,25 @@
   Created Feb.13.2024
  */
 #include <WiFiS3.h>
-#include "Arduino_LED_Matrix.h"
-#include "arduino_secrets.h" 
+#include <cstring> // For strchr
 
-ArduinoLEDMatrix matrix;
-const uint32_t happy[] = {
-    0x19819,
-    0x80000001,
-    0x81f8000
-};
-const uint32_t heart[] = {
-    0x3184a444,
-    0x44042081,
-    0x100a0040
-};
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int status = WL_IDLE_STATUS;     // the WiFi radio's status
-IPAddress ip(128, 97, 13, 156);
-WiFiClient client;
+#include "LedMatrixConfig.h";
+#include "WiFiFunctions.h";
 
-String serverIP = "128.97.13.180"; // Server IP address
-int serverPort = 5000;             // Server port
+WiFiServer server(80);
 
-//Only one DAC output available (Pin A0), so it's not defined here
-const int interruptPin = 2; // Digital pin used for the interrupt
-
+// Global variables for interrupt handling
+unsigned long currentMillis = millis();
 volatile bool interruptTriggered = false;
-unsigned long interruptTime = 0;
-unsigned long lastInterruptTime = 0; // To prevent debounce effect
+unsigned long lastInterruptTime = 0; // Time of the last interrupt
+unsigned long prevInterruptTime = 0; // Time of the interrupt before the last one
+const int interruptPin = 2; // Example interrupt pin, adjust according to your setup
 
-volatile int outputValue = 0; // Variable to store the analog output value, volatile because it is changed in the loop and read in the ISR
-volatile long signalDuration = 1000; // Duration to maintain the output signal level in milliseconds (e.g., 5000ms or 5 seconds)
-
+// Global variables for Analog write
+unsigned int signalDuration = 1000; // Duration for signal in milliseconds
+unsigned int highLevel = 0;
+unsigned int lowLevel  = 0;
+int outputValue = 4096; // Example output value for 12-bit resolution
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -46,37 +31,16 @@ void setup() {
   }
 
   matrix.begin(); // enable LED matrix
-  matrix.loadFrame(heart);
+  LEDmouth();
+  matrix.renderBitmap(frame, 8, 12);
 
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
+  connectWiFi();
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-
-  WiFi.config(ip);
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-
-  // you're connected now, so print out the data:
-  Serial.print("You're connected to the network");
-  printCurrentNet();
-  printWifiData();
-  matrix.loadFrame(happy);
+  server.begin();
+  Serial.println("Server started");
+  LEDleftEye();
+  LEDrightEye();
+  matrix.renderBitmap(frame, 8, 12);
 
   // Analog setup
   analogWriteResolution(12); //change to 12-bit resolution
@@ -84,98 +48,92 @@ void setup() {
 
   // Interrupt setup
   pinMode(interruptPin, INPUT_PULLUP); // Set the interrupt pin as input with pull-up
-
-  // Attach an interrupt to the interrupt pin. The interrupt is triggered on the falling edge (HIGH to LOW transition)
-  attachInterrupt(digitalPinToInterrupt(interruptPin), onInterrupt, FALLING);
-  Serial.println("Attached interrupt pin");
-  Serial.println(interruptTriggered);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), onInterrupt, FALLING); // Attach interrupt
 }
 
 void loop() {
-
   if (interruptTriggered) {
     setOutputLevel();
-    interruptTriggered = false;
+    interruptTriggered = false; // Reset the flag after handling the interrupt
   } else {
-    if (client...){}
-    attachInterrupt(digitalPinToInterrupt(interruptPin), onInterrupt, FALLING);
+    clientOperation();
   }
-
-//  if (!interruptTriggered && client.connect(serverIP.c_str(), serverPort)) {
-//      Serial.println("Client connected");
-//      client.println("Connected");
-//      client.stop();
-//  }
-
 }
 
 void onInterrupt() {
+  prevInterruptTime = lastInterruptTime; // Store the previous interrupt time
+  lastInterruptTime = millis(); // Update the last interrupt time
   interruptTriggered = true;
-  interruptTime = millis();
-  // Temporarily disable the interrupt to prevent re-triggering during the signal duration
+
+  // Temporarily disable the interrupt
   detachInterrupt(digitalPinToInterrupt(interruptPin));
 }
 
 void setOutputLevel() {
-      analogWrite(A0, outputValue);
-      matrix.loadFrame(heart);
-      
-      delay(signalDuration);
+  analogWrite(A0, outputValue);
+  LEDwink();
+  matrix.renderBitmap(frame, 8, 12);
+  delay(500);
+  analogWrite(A0, 0);
+  LEDleftEye();
+  LEDrightEye();
+  matrix.renderBitmap(frame, 8, 12);
 
-      analogWrite(A0, 0);
-      matrix.loadFrame(happy);
+  // Re-enable the interrupt after handling to be ready for the next one
+  attachInterrupt(digitalPinToInterrupt(interruptPin), onInterrupt, FALLING);
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// WiFi functions
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void printWifiData() {
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  
-  Serial.println(ip);
-
-  // print your MAC address:
-  byte mac[6];
-  WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
-  printMacAddress(mac);
-}
-
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);
-  Serial.print("BSSID: ");
-  printMacAddress(bssid);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-
-  // print the encryption type:
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption, HEX);
-  Serial.println();
-}
-
-void printMacAddress(byte mac[]) {
-  for (int i = 0; i < 6; i++) {
-    if (i > 0) {
-      Serial.print(":");
-    }
-    if (mac[i] < 16) {
-      Serial.print("0");
-    }
-    Serial.print(mac[i], HEX);
+// Function to get the time difference between the previous two interrupts
+unsigned long getTimeDifferenceBetweenInterrupts() {
+  if (prevInterruptTime == 0) {
+    // If there's no previous interrupt recorded, return 0
+    return 0;
+  } else {
+    return lastInterruptTime - prevInterruptTime;
   }
-  Serial.println();
+}
+
+void clientOperation() {
+
+  unsigned long timeout = (getTimeDifferenceBetweenInterrupts() - signalDuration);
+
+  WiFiClient client = server.available();
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    unsigned long initMillis = millis();
+    
+    while (client.connected()) { // loop while the client's connected
+      currentMillis = millis();  // update current time
+      char buf[100];             // make a buffer to hold incoming data from the client
+      if (client.available()) { // if there's bytes to read from the client,
+        // client.read takes two inputs--unsigned char* buffer and its size. Output size of the message received from client
+        int c = client.read((unsigned char*)buf, 99);
+        client.println("message received");
+        buf[c] = 0;          // message needs to be terminated with a zero
+        Serial.println(buf); // print message to serial window
+        if (!strchr(buf, ',')) {
+          client.println("Message wrong!");
+          break;
+        }
+
+        sscanf(buf, "%d,%d,%d", highLevel, lowLevel, signalDuration);
+        Serial.print("High Level = ");
+        Serial.println(highLevel);
+        Serial.print("Low Level = ");
+        Serial.println(lowLevel);
+        Serial.print("On time = ");
+        Serial.println(signalDuration);
+
+        break;
+
+      } else if (currentMillis - initMillis >= timeout) {
+          client.println("TIMEOUT");
+          break;
+        }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+  
 }
