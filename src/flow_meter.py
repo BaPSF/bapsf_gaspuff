4b50198ab51fd51d1c21a07789c737437c5e74ce
Jia <acoruscalamuslinn@gmail.com>
2025-01-07 09:54:48 -0800

    remove unneccessary files. Modify FlowMeter class for better readability using AI.

--- src/flow_meter.py
+++ src/flow_meter.py
@@ -3,13 +3,13 @@ from sensirion_shdlc_sfc5xxx import Sfc5xxxShdlcDevice, Sfc5xxxScaling, \
     Sfc5xxxValveInputSource, Sfc5xxxUnitPrefix, Sfc5xxxUnit, \
     Sfc5xxxUnitTimeBase, Sfc5xxxMediumUnit
 import time
-import RPi.GPIO as GPIO
 import numpy as np
 
 class FlowMeter(object):
     """
     This class represents the flowmeter device and handles its I/O with the APIs provided by manufacturer
-    Sensirion."""
+    Sensirion.
+    """
     def __init__(self, port='/dev/ttyUSB0', baudrate=460800, slave_address=2):
         """
         Initialize connection to the flow meter and other configurations.
@@ -23,6 +23,10 @@ class FlowMeter(object):
         """
         self.port = ShdlcSerialPort(port=port, baudrate=baudrate) # setup serial port
         self.device = Sfc5xxxShdlcDevice(ShdlcConnection(self.port), slave_address=slave_address)
+        
+        # Print device information upon initialization
+        self._print_device_info()
+        
         self.device.activate_calibration(3) # specify calibration file index in list; default now on Helium (3)
         # set units
         self.unit = Sfc5xxxMediumUnit(
@@ -32,6 +36,22 @@ class FlowMeter(object):
         )
         self.device.set_user_defined_medium_unit(self.unit)
 
+    def _print_device_info(self):
+        """Print device information and available calibration blocks."""
+        print(f"\nFlow meter at {self.port.port} (slave address: {self.device.slave_address})")
+        print("Version:", self.device.get_version())
+        print("Product Name:", self.device.get_product_name())
+        print("Article Code:", self.device.get_article_code())
+        print("Serial Number:", self.device.get_serial_number())
+        
+        print("\nAvailable gas calibration blocks:")
+        for i in range(self.device.get_number_of_calibrations()):
+            if self.device.get_calibration_validity(i):
+                gas = self.device.get_calibration_gas_description(i)
+                fullscale = self.device.get_calibration_fullscale(i)
+                unit = self.device.get_calibration_gas_unit(i)
+                print(f" - {i}: {fullscale:.2f} {unit} {gas}")
+        print()
 
     def set_baudrate(self, baudrate):
         """
@@ -87,122 +107,3 @@ class FlowMeter(object):
             samples.append(self.device.read_measured_value(Sfc5xxxScaling.USER_DEFINED))
         
         return samples
-
-
-"""
-This class represents a high-level interface for controlling the flow reading system,
-including triggering and data acquisition.
-"""
-def initialize(self, gpio_channel) -> None:
-    """
-    Initialize the flow reader system, including trigger box and flow meter connection.
-    
-    Parameters
-    ----------
-    gpio_channel : GPIO pin number for input of the trigger signal.
-    """
-    # self.wavegen = wavegen_control(server_ip_addr='192.168.1.13')
-    self.flow_meter = FlowMeter()
-    GPIO.setmode(GPIO.BCM) # set GPIO indexing convention to BCM
-    GPIO.setup(gpio_channel, GPIO.IN)
-    self.gpio_channel = gpio_channel
-
-# def set_waveform(self, function, voltages, freq=None, offset=None):
-    # if function == 'USER':
-        # self.wavegen.send_dac_data(generate_pulse().astype('>i2'))
-        # self.wavegen.send_dac_data(generate_pulse(freq, offset).astype('>i2'))
-    # self.wavegen.function = function
-    # self.wavegen.voltage_level = voltages[0], voltages[1]
-    # if freq is not None:
-        #   self.wavegen.frequency = freq
-    # if offset is not None:
-        # self.wavegen.DCoffset = offset
-
-# def burst_mode(self, ncycles):
-    # self.wavegen.burst(enable=True, ncycles=ncycles, phase=0)
-    
-
-def acquire(self, duration, acquisition_limit=100):
-    """
-    Acquire flow rate measurements from the flow meter for a fixed duration at trigger.
-    Currently an upper limit of the number of acquisitions is in place.
-    
-    Parameters
-    ----------
-    duration : Duration of a single acquisition in seconds. This should not exceed the period of
-    plasma discharge to avoid malfunctioning.
-    acquisition_limit : Maximum number of acquisition the command can perform.
-    """
-    shot_counts = 0
-    try:
-        while shot_counts <= acquisition_limit:
-            print('waiting for signals...')
-            GPIO.wait_for_edge(self.gpio_channel, GPIO.RISING) # stop the code until receiving a trigger
-            #time.sleep(.1)
-            t = time.time()
-            #readings = np.array(self.flow_meter.get_reading(duration))
-            readings = np.array(self.flow_meter.get_reading_single_cycle(duration))
-            #readings = np.array(self.flow_meter.get_single_buffer())
-            np.savetxt(f'/home/pi/flow_meter/data/output_single_cycle_{shot_counts}.csv', readings)
-            print('shot count {}'.format(shot_counts))
-            print(f'shot interval {time.time()-t}')
-            shot_counts += 1
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        print('exit on ctrl-C keyboard interrupt')
-    except:
-        GPIO.cleanup()
-        print('an error occured')
-    print('Maximum number of shot records reached!')
-    GPIO.cleanup()
-
-
-# below is the standalone implementation of flow meter reading for reference
-'''
-with ShdlcSerialPort(port='/dev/ttyUSB0', baudrate=460800) as port:   
-    device = Sfc5xxxShdlcDevice(ShdlcConnection(port), slave_address=2)
-    
-    # select calibration
-    print('activate calibration...')
-    device.activate_calibration(3)
-    
-    # set units
-    unit = Sfc5xxxMediumUnit(
-        Sfc5xxxUnitPrefix.ONE,
-        Sfc5xxxUnit.STANDARD_LITER,
-        Sfc5xxxUnitTimeBase.MINUTE
-    )
-
-    device.set_user_defined_medium_unit(unit)
-
-    # read flow value for 10s
-    # try with single value reading
-    print('start acquiring...')
-    
-    # an implementation with buffer reading
-    read_time = []
-    reading = []
-    t = time.time()
-    buffer = device.read_measured_value_buffer(Sfc5xxxScaling.USER_DEFINED) # dump what's already inside the buffer
-    while len(reading) <= 3000:
-        buffer = device.read_measured_value_buffer(Sfc5xxxScaling.USER_DEFINED)
-        reading.extend(buffer.values)
-        if buffer.lost_values != 0:
-            print('lost values detected!')
-        #read_time.extend([t * 0.001 for t in range(counter, counter + len(buffer.values))])
-        #print(buffer.lost_values)
-    print('execution time:', time.time()-t)
-    
-    
-    output = open('flow_reading.txt', 'w', encoding='utf-8')
-    #output_time = open('sampling_time.txt', 'w', encoding='utf-8')
-    for r in reading:
-        output.write(str(r))
-        output.write("\n")
-    #for t in read_time:
-        #output_time.write(str(t))
-        #output_time.write("\n")
-    print('success')
-    output.close()
-    #output_time.close()
-'''
--- src/plot.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = np.loadtxt('/home/pi/flow_meter/data/output_0.csv')
-plt.plot(data)
-plt.show()
